3c9b65fe063bb03799449557e637476f5f6cabd8
manikanta katakam <90960975+manikantakatakam@users.noreply.github.com>
2024-04-23 15:17:13 +0530

    Update MerchantController.js

--- Controllers/MerchantController.js
+++ Controllers/MerchantController.js
@@ -26,23 +26,20 @@ const createOffer = asyncHandler(async(req, res) => {
     }
 });
 
-
+// adding offer to merchant
 const addOffer = asyncHandler(async(req, res) => {
     const merchantName = req.params.merchantName;
   const offerData = req.body;
 
   try {
-      // Find the merchant by name
       const merchant = await Merchant.findOne({ MerchantName: merchantName });
 
       if (!merchant) {
           return res.status(404).json({ message: 'Merchant not found' });
       }
       
-      // Add the new offer to the offers array
       merchant.offers.push(offerData);
 
-      // Save the updated merchant
       await merchant.save();
 
       res.status(200).json({ message: 'Offer added successfully' });
@@ -52,22 +49,22 @@ const addOffer = asyncHandler(async(req, res) => {
   }
 });
 
+// updating offer
 const updateOffer = asyncHandler(async(req, res) => {
     const offerId = req.params.offerId;
     const updatedOfferData = req.body;
 
     try {
-        // Construct the update object using $set for individual fields
+        
         const updateObj = {};
         Object.keys(updatedOfferData).forEach(key => {
             updateObj[`offers.$.${key}`] = updatedOfferData[key];
         });
 
-        // Find the offer by its ID and update specific fields
         const updatedOffer = await Merchant.findOneAndUpdate(
-            { 'offers.offerID': offerId }, // Find by offerID within the offers array
-            { $set: updateObj }, // Update specific fields of the matching offer
-            { new: true } // Return the updated document
+            { 'offers.offerID': offerId }, 
+            { $set: updateObj }, 
+            { new: true } 
         );
 
         if (!updatedOffer) {
@@ -81,16 +78,16 @@ const updateOffer = asyncHandler(async(req, res) => {
     }
 });
 
-
+//deleting offer
 const deleteOffer = asyncHandler(async(req, res) => {
     const offerId = req.params.offerId;
 
     try {
-        // Find the offer by its ID and remove it
+       
         const updatedMerchant = await Merchant.findOneAndUpdate(
-            { 'offers.offerID': offerId }, // Find by offerID within the offers array
-            { $pull: { offers: { offerID: offerId } } }, // Remove the offer from the offers array
-            { new: true } // Return the updated document
+            { 'offers.offerID': offerId }, 
+            { $pull: { offers: { offerID: offerId } } },
+            { new: true } 
         );
 
         if (!updatedMerchant) {
